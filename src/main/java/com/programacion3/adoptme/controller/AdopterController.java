73b94828b8e1ed8cb5383307ed834a8f85b205ef
Daniela Melian <meliandaani@gmail.com>
2025-10-17 21:26:01 -0300

    algoritmos dfs y bfs para encontrar caminos entre los refugios

--- src/main/java/com/programacion3/adoptme/controller/AdopterController.java
+++ src/main/java/com/programacion3/adoptme/controller/AdopterController.java
@@ -1,16 +1,27 @@
 package com.programacion3.adoptme.controller;
 
 import com.programacion3.adoptme.domain.Adopter;
-import com.programacion3.adoptme.repo.AdopterRepository;
+import com.programacion3.adoptme.repo.AdopterRepository; // usa 'repo' si ese es tu package
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/adopters")
+@RequiredArgsConstructor
 public class AdopterController {
-    private final AdopterRepository repo;
-    public AdopterController(AdopterRepository repo) { this.repo = repo; }
+
+    private final AdopterRepository adopterRepository;
 
     @GetMapping
-    public List<Adopter> all() { return repo.findAll(); }
+    public List<Adopter> getAll() {
+        return adopterRepository.findAll();
+    }
+
+
+    @PostMapping
+    public Adopter create(@RequestBody Adopter adopter) {
+        return adopterRepository.save(adopter);
+    }
 }
